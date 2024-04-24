56dca7c31e8663dad6429b1be9150909789c16af
Abel Rivas Bautista <abel.rivas@Imperial.local>
2024-04-24 14:33:36 -0400

    add views, nav component and routes

---
+++ src/context/Context.jsx
@@ -0,0 +1,7 @@
+const Context = () => {
+  return (
+    <div>Context</div>
+  )
+}
+
+export default Context
---
+++ src/views/index.jsx
@@ -0,0 +1,5 @@
+export { default as Home } from './Home'
+export { default as Pizza } from './Pizza'
+export { default as Details } from './Details'
+export { default as Cart } from './Cart'
+export { default as NotFound } from './NotFound'
